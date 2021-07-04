01256f8dad9212f0f0c4326977e2962cae136e2c
johnny <johnny1194@hotmail.com>
2021-07-04 09:04:40 -0400

    Completed assignment

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+
+def prime?(num)
+    if num<0 || num==0 || num ==1
+        return false
+    else
+        (2..num-1).to_a.all? do |potential_factor|
+            num % potential_factor != 0
+        end
+    end
+end
